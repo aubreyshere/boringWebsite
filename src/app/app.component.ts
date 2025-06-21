ea8362f04d5f098e16a4625f712345280da9cecb
aubreyshere <aubreyh156@gmail.com>
2025-06-21 00:23:25 -0700

    nav bar, bio, and projects setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BioComponent } from './bio/bio.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BioComponent, ProjectsComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
